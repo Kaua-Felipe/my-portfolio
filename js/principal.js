e94d6aa159b85dc1f162f30b46ce0785e698afeb
Kauã <kauafelipeoficial191@gmail.com>
2022-07-01 13:28:22 -0300

    Adicionando animações

--- js/principal.js
+++ js/principal.js
@@ -12,7 +12,7 @@ function typeWriter(elemento) {
 const titulo = document.querySelector("h3")
 typeWriter(titulo)
 
-/* Alteração Altomática da Escrita */
+/* Alteração Automática da Escrita */
 $("input[name=name-people]").on("keyup", function() {
     $("span#name-people").html($(this).val())
 })
@@ -23,3 +23,62 @@ var loader = document.getElementById("preloader")
 window.addEventListener("load", () => {
     loader.style.display = "none"
 })
+
+/* Animar ao Scroll */
+const debounce = function(func, wait, immediate) {
+    let timeout
+    return function(...args) {
+        const context = this
+        const later = () => {
+            timeout = null
+            if (!immediate) func.apply(context, args)
+        }
+        const callNow = immediate && !timeout
+        clearTimeout(timeout)
+        timeout = setTimeout(later, wait)
+        if (callNow) func.apply(context, args)
+    }
+}
+
+const target = document.querySelectorAll("[data-animation]")
+const animationClass = "animate"
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 9) / 10)
+    target.forEach((element) => {
+        if(windowTop > element.offsetTop) {
+            element.classList.add(animationClass)
+        }
+        else {
+            element.classList.remove(animationClass)
+        }
+    })
+}
+
+animeScroll()
+if(target.length) {
+    window.addEventListener("scroll", debounce(() => {
+        animeScroll()
+    }, 10))
+}
+
+/* Animar Navegação ao Scroll */
+// var nav = document.querySelector("nav#main-nav")
+// window.addEventListener("scroll", () => {
+//     nav.style.top = "-100px"
+// })
+// while(true) {
+
+//     break
+// }
+var prevScrollpos = window.pageYOffset
+var nav = document.querySelector("nav#main-nav")
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset
+    if(prevScrollpos > currentScrollPos) {
+        nav.style.top = "0"
+    } else {
+        nav.style.top = "-100px"
+    }
+    prevScrollpos = currentScrollPos
+}
